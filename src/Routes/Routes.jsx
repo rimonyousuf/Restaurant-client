7b30835fe0b1c5334feb6ff99d5afa09a0ba1182
Rimon <rimonyousuf00@gmail.com>
2024-05-11 09:45:32 +0600

    Menu item in database has been added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,6 @@
 import {
-    createBrowserRouter,
-  } from "react-router-dom";
+  createBrowserRouter,
+} from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
 import Menu from "../Pages/Menu/Menu/Menu";
@@ -12,50 +12,59 @@ import Secret from "../Pages/Shared/Secret/Secret";
 import Dashboard from "../Layout/Dashboard";
 import Cart from "../Pages/Dashboard/Cart/Cart";
 import AllUsers from "../Pages/Dashboard/AllUsers/AllUsers";
+import AddItems from "../Pages/Dashboard/AddItems/AddItems";
+import AdminRoute from "./AdminRoute";
 
-  export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      children: [
-        {
-            path: "/",
-            element: <Home></Home>
-        },
-        {
-          path: 'menu',
-          element: <Menu></Menu>
-        },
-        {
-          path: 'order/:category',
-          element: <Order></Order>
-        },
-        {
-          path: 'login',
-          element: <Login></Login>
-        },
-        {
-          path: 'signup',
-          element: <Signup></Signup>
-        },
-        {
-          path: 'secret',
-          element: <PrivateRoutes><Secret></Secret></PrivateRoutes>
-        },
-      ]
-    },
-    {
-      path: 'dashboard',
-      element: <PrivateRoutes><Dashboard></Dashboard></PrivateRoutes>,
-      children: [
-        {
-          path: 'cart',
-          element: <Cart></Cart>
-        },
-        {
-          path: 'users',
-          element: <AllUsers></AllUsers>
-        },
-      ]
-    }
-  ]);
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: 'menu',
+        element: <Menu></Menu>
+      },
+      {
+        path: 'order/:category',
+        element: <Order></Order>
+      },
+      {
+        path: 'login',
+        element: <Login></Login>
+      },
+      {
+        path: 'signup',
+        element: <Signup></Signup>
+      },
+      {
+        path: 'secret',
+        element: <PrivateRoutes><Secret></Secret></PrivateRoutes>
+      },
+    ]
+  },
+  {
+    path: 'dashboard',
+    element: <PrivateRoutes><Dashboard></Dashboard></PrivateRoutes>,
+    children: [
+      // normal user routes
+      {
+        path: 'cart',
+        element: <Cart></Cart>
+      },
+
+      // admin only routes
+      {
+        path: 'addItems',
+        element: <AdminRoute><AddItems></AddItems></AdminRoute>
+      },
+      {
+        path: 'users',
+        element: <AdminRoute><AllUsers></AllUsers></AdminRoute>
+      },
+    ]
+  }
+]);
